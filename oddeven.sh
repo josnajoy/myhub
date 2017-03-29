bed0b158e7a73eb4671f7729d2a9f7f20abf9152
= <= josnaelsa@gmail.co m>
2017-03-29 11:56:38 +0530

    committed

--- oddeven.sh
+++ oddeven.sh
@@ -6,18 +6,17 @@ allsum=0
 i=1
 while [ $i -le $n ] 
 do
-
-	echo "Enter the number"
-	read num
-	c=`expr $num % 2`
-	if [  $c -eq 0 ]
-	then 
-		evensum=`expr $evensum + $num`
-	else
-		oddsum=`expr $oddsum + $num`
-	fi
-	allsum=`expr $allsum + $num`
-	i=`expr $i + 1`	
+echo "Enter the number"
+read num
+c=`expr $num % 2`
+if [  $c -eq 0 ]
+then 
+evensum=`expr $evensum + $num`
+else
+oddsum=`expr $oddsum + $num`
+fi
+allsum=`expr $allsum + $num`
+i=`expr $i + 1`	
 done
 echo "Sum of even numbers = $evensum"
 echo "Sum of odd numbers = $oddsum"
